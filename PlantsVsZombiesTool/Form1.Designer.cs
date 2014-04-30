9d740dbdb2ad06aa198d095a0e7fcf66d344972b
zhangsanbin <sanbinzhang@163.com>
2014-04-30 15:51:48 +0800

    Upload all files
    Upload all files

---
+++ PlantsVsZombiesTool/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace PlantsVsZombiesTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnGet = new System.Windows.Forms.Button();
+            this.btnMoney = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtLev = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(12, 72);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(75, 23);
+            this.btnGet.TabIndex = 0;
+            this.btnGet.Text = "Sun";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // btnMoney
+            // 
+            this.btnMoney.Location = new System.Drawing.Point(12, 101);
+            this.btnMoney.Name = "btnMoney";
+            this.btnMoney.Size = new System.Drawing.Size(75, 23);
+            this.btnMoney.TabIndex = 0;
+            this.btnMoney.Text = "Money";
+            this.btnMoney.UseVisualStyleBackColor = true;
+            this.btnMoney.Click += new System.EventHandler(this.btnMoney_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(12, 43);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtLev
+            // 
+            this.txtLev.Location = new System.Drawing.Point(12, 16);
+            this.txtLev.Name = "txtLev";
+            this.txtLev.Size = new System.Drawing.Size(75, 21);
+            this.txtLev.TabIndex = 2;
+            this.txtLev.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.txtLev);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnMoney);
+            this.Controls.Add(this.btnGet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.Button btnMoney;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtLev;
+    }
+}
+
---
+++ PlantsVsZombiesTool/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PlantsVsZombiesTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //启动无线阳光
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            if (Helper.GetPidByProcessName(processName) == 0)
+            {
+                MessageBox.Show("哥们启用之前游戏总该运行吧！");
+                return;
+            }
+            if (btnGet.Text == "启用-阳光无限")
+            {
+                timer1.Enabled = true;
+                btnGet.Text = "关闭-阳光无限";
+            }
+            else
+            {
+                timer1.Enabled = false;
+                btnGet.Text = "启用-阳光无限";
+            }
+        }
+
+        private void btnMoney_Click(object sender, EventArgs e)
+        {
+            if (Helper.GetPidByProcessName(processName) == 0)
+            {
+                MessageBox.Show("哥们启用之前游戏总该运行吧！");
+                return;
+            }
+            if (btnMoney.Text == "启用-金钱无限")
+            {
+                timer2.Enabled = true;
+                btnMoney.Text = "关闭-金钱无限";
+            }
+            else
+            {
+                timer2.Enabled = false;
+                btnMoney.Text = "启用-金钱无限";
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Helper.GetPidByProcessName(processName) == 0)
+            {
+                timer1.Enabled = false;
+                btnGet.Text = "启用-阳光无限";
+            }
+            int address = ReadMemoryValue(baseAddress);             //读取基址(该地址不会改变)
+            address = address + 0x768;                              //获取2级地址
+            address = ReadMemoryValue(address);
+            address = address + 0x5560;                             //获取存放阳光数值的地址
+            WriteMemory(address, 0x1869F);                          //写入数据到地址（0x1869F表示99999）
+            timer1.Interval = 1000;
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (Helper.GetPidByProcessName(processName) == 0)
+            {
+                timer2.Enabled = false;
+                btnMoney.Text = "启用-金钱无限";
+            }
+            int address = ReadMemoryValue(baseAddress);             //读取基址(该地址不会改变)
+            address = address + 0x82C;                              //获取2级地址
+            address = ReadMemoryValue(address);
+            address = address + 0x28;                               //得到金钱地址
+            WriteMemory(address, 0x1869F);                          //写入数据到地址（0x1869F表示99999）
+            timer2.Interval = 1000;
+
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if (Helper.GetPidByProcessName(processName) == 0)
+            {
+                MessageBox.Show("哥们启用之前游戏总该运行吧！");
+                return;
+            }
+            int address = ReadMemoryValue(baseAddress);             //读取基址(该地址不会改变)
+            address = address + 0x82C;                              //获取2级地址
+            address = ReadMemoryValue(address);
+            address = address + 0x24;
+            int lev = 1;
+            try
+            {
+                lev = int.Parse(txtLev.Text.Trim());
+            }
+            catch
+            {
+                MessageBox.Show("输入的关卡格式不真确！默认设置为1");
+            }
+
+            WriteMemory(address, lev);
+
+        }
+
+        //读取制定内存中的值
+        public int ReadMemoryValue(int baseAdd)
+        {
+            return Helper.ReadMemoryValue(baseAdd, processName);
+        }
+
+        //将值写入指定内存中
+        public void WriteMemory(int baseAdd, int value)
+        {
+            Helper.WriteMemoryValue(baseAdd, processName, value);
+        }
+
+        private int baseAddress = 0x006A9EC0;           //游戏内存基址
+        private string processName = "PlantsVsZombies"; //游戏进程名字
+
+    }
+}
